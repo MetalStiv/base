2f05df10953851aadaf14629abf6fcc6d558fba1
MetalStiv <stepangrishunov@yandex.com>
2025-02-19 20:25:26 +0000

    add swagger example

--- rebuild.sh
+++ rebuild.sh
@@ -1,11 +1,11 @@
-docker-compose down
+docker compose down
 #./genRsaKeys.sh
-npx tsc user-microservice/src/*.ts --outDir user-microservice/dist --downlevelIteration
+yarn tsc user-microservice/src/*.ts --outDir user-microservice/dist --downlevelIteration
 docker build -t user_microservice:1.0 ./user-microservice
 
-npx tsc transaction-microservice/src/*.ts --outDir transaction-microservice/dist --downlevelIteration
+yarn tsc transaction-microservice/src/*.ts --outDir transaction-microservice/dist --downlevelIteration
 docker build -t transaction_microservice:1.0 ./transaction-microservice
 
 yarn --cwd ./frontend build
 
-docker-compose up -d
+docker compose up -d
